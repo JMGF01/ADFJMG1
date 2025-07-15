ad6d49bfba9b49e50b6ef96fa311c5d8fe345a05
JMGF01 <quelel@gmail.com>
2025-07-15 14:00:23 +0200

    navigation view

--- app/src/main/java/edu/adf/adfjmg1/MainMenuActivity.kt
+++ app/src/main/java/edu/adf/adfjmg1/MainMenuActivity.kt
@@ -2,10 +2,15 @@ package edu.adf.adfjmg1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 /**
  *  ESTA ES LA ACTIVIDAD DE INICIO
@@ -13,19 +18,58 @@ import androidx.core.view.WindowInsetsCompat
  *  EN UN FUTURO, PONDREMOS UN MENÚ HAMBURGUESA / LATERAL
  *  DE MOMENTO, LO HACEMOS CON INTENTS
  */
-class MainMenuActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    var menuvisible: Boolean = false // Se usa para controlar el estado del menú de la aplicación.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        this.drawerLayout =  findViewById<DrawerLayout>(R.id.drawer)
+        this.navigationView = findViewById<NavigationView>(R.id.navigationView)
+
+        // en esta actividad (this) escuchamos la selección sobre el menú Navigation
+        this.navigationView.setNavigationItemSelectedListener(this)
+
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true) // dibuja el ícono de menú
+        this.supportActionBar?.setHomeAsUpIndicator(R.drawable.outline_menu_24) // le digo que me dibuje la hamburguesa
+
+//        // VAMOS A LANZAR LA ACTIVIDAD IMC
+//        val intent = Intent(this, ImcActivity::class.java)
+//        startActivity(intent)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            android.R.id.home -> {// Esta clase R no es la de mi proyecto, si no la de Android.
+                Log.d("MiImcActivity", "Botón Hamburguesa tocado")
+                if (this.menuvisible) {
+                    this.drawerLayout.closeDrawers()
+                    this.menuvisible = false
+                } else {
+                    this.drawerLayout.openDrawer(GravityCompat.START)
+                    this.menuvisible = true
+                }
+            }
         }
 
-        // VAMOS A LANZAR LA ACTIVIDAD IMC
-        val intent = Intent(this, ImcActivity::class.java)
-        startActivity(intent)
+        return super.onOptionsItemSelected(item)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d("MiImcActivity", "Opción ${item.order} seleccionada")
+        this.drawerLayout.closeDrawers()
+        this.menuvisible = false
+
+        return true
+    }
+
+
 }
+
